beba68b3fe9a42ea2d9bc3955644b113eff3cae7
Ikars Melnalksnis <ikars.melnalksnis@lbtu.lv>
2026-05-12 22:11:41 +0300

    Build

--- dist/assets/content.ts-loader-hyvhGwHE.js
+++ dist/assets/content.ts-loader-hyvhGwHE.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/content.ts-Je8_Yh1L.js")
+      chrome.runtime.getURL("assets/content.ts-DktODrNi.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
